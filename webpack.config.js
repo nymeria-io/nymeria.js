8ea7918d0c5206188125afc694c9019baee58dcc
Nymeria <dev@nymeria.io>
2022-05-03 07:10:55 -0700

    Moved to webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const browser = {
+  target: "web",
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+    library: "nymeria",
+    libraryTarget: "umd",
+    globalObject: "this",
+    umdNamedDefine: true,
+  },
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
+  },
+  plugins: [],
+  resolve: {
+    extensions: [".js"],
+  },
+};
+
+module.exports = [browser];
